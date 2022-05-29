a4728f854985cbf8b959daf4ef560eb21d1c234e
laxmi <laxmi.mahato.lm.9803@gmail.com>
2022-05-29 08:10:11 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import User from './components/User';
-import AddUser from './components/AddUser';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import User from "./components/User";
+import AddUser from "./components/AddUser";
+import "./App.css";
 
 const App = () => {
-  
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -14,146 +13,159 @@ const App = () => {
   //retrieving data from api
   const fetchData = async () => {
     await fetch(`https://mockend.com/implaxmi/react-crud/users`)
-    .then((res) => res.json())
-    .then((data) => setUsers(data))
-    .catch((err) => {
-      console.log(err);
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  //adding data
+  const onAdd = async (name, capacity, status, image) => {
+    if (status === "on") {
+      status = true;
+    } else {
+      status = false;
+    }
+    await fetch(`https://mockend.com/implaxmi/react-crud/users`, {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        capacity: capacity,
+        status: status,
+        image: image,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=utf-8",
+      },
     })
-}
+      .then((res) => {
+        if (res.status !== 201) {
+          return;
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        setUsers((users) => [...users, data]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
- //adding data 
- const onAdd = async (name, capacity, status, image) => {
-   if(status === 'on') {
-     status = true;
-   } else {
-     status = false;;
-   }
-  await fetch(`https://mockend.com/implaxmi/react-crud/users`, {
-    method: 'POST',
-    body: JSON.stringify({
-      name: name,
-      capacity : capacity,
-      status: status,
-      image: image
-    }), 
-    headers: {
-      "Content-type": "application/json; charset=utf-8"
-    }
-  })
-  .then((res) => {
-    if(res.status !== 201) {
-      return
-    } else {
-      return res.json();
-    }
-  })
-  .then((data) => {
-    setUsers((users) => [...users, data]);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-}
-
-
-//deleting data 
-const onDelete = async (id) => {
-  await fetch(`https://mockend.com/implaxmi/react-crud/users/${id}`, {
-    method: 'DELETE'
-  })
-  .then((res) => {
-    if(res.status !== 200) {
-      return;
-    } else {
-      setUsers(users.filter((user) => {
-        return user.id !== id
-      }));
-    }
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-}
-
+  //deleting data
+  const onDelete = async (id) => {
+    await fetch(`https://mockend.com/implaxmi/react-crud/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          return;
+        } else {
+          setUsers(
+            users.filter((user) => {
+              return user.id !== id;
+            })
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-     onAdd(e.target.name.value,e.target.capacity.value, e.target.status.value, e.target.image.value);
-     e.target.name.value = "";
-     e.target.capacity.value = "";
-     e.target.status.checked = "";
-     e.target.image.value = "";
-}
+    onAdd(
+      e.target.name.value,
+      e.target.capacity.value,
+      e.target.status.value,
+      e.target.image.value
+    );
+    e.target.name.value = "";
+    e.target.capacity.value = "";
+    e.target.status.checked = "";
+    e.target.image.value = "";
+  };
 
-const onEdit = async (id, name, capacity, status, image) => {
-  if(status === 'on') {
-    status = true;
-  } else {
-    status = false;;
-  }
-  await fetch(`https://mockend.com/implaxmi/react-crud/users/${id}`, {
-    method: "PUT",
-    body: JSON.stringify({
-      name: name,
-      capacity: capacity,
-      status: status,
-      image: image
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
+  const onEdit = async (id, name, capacity, status, image) => {
+    if (status === "on") {
+      status = true;
+    } else {
+      status = false;
     }
-  })
-    .then((response) => {
-      if (response.status !== 200) {
-        return;
-      } else {
-        return response.json();
-      }
+    await fetch(`https://mockend.com/implaxmi/react-crud/users/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        name: name,
+        capacity: capacity,
+        status: status,
+        image: image,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
     })
-    .then((data) => {
-      // setUsers((users) => [...users, data]);
-      const updatedUsers = users.map((user) => {
-        if (user.id === id) {
-          user.name = name;
-          user.capacity = capacity; 
-          user.status = status;
-          user.image = image;
+      .then((response) => {
+        if (response.status !== 200) {
+          return;
+        } else {
+          return response.json();
         }
+      })
+      .then((data) => {
+        // setUsers((users) => [...users, data]);
+        const updatedUsers = users.map((user) => {
+          if (user.id === id) {
+            user.name = name;
+            user.capacity = capacity;
+            user.status = status;
+            user.image = image;
+          }
 
-        return user;
-      });
-      setUsers((users) => updatedUsers);
-    })
-    .catch((error) => console.log(error));
-};
-
+          return user;
+        });
+        setUsers((users) => updatedUsers);
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <>
-    <div className="container">
-      <h4 className="header">Create Table</h4>
-      <hr></hr>
-      <div className="form-container">
-      <AddUser handleSubmit={handleSubmit} />
+      <div className="container">
+        <h4 className="header">Create Table</h4>
+        <hr></hr>
+        <div className="form-container">
+          <AddUser handleSubmit={handleSubmit} />
+        </div>
       </div>
-    </div>
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Capacity</th>
-          <th>Status</th>
-          <th>Image</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Capacity</th>
+            <th>Status</th>
+            <th>Image</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
       </table>
       {users.map((user) => (
-        <User key={user.id} id={user.id} name={user.name} capacity={user.capacity} status={user.status} image={user.image} onEdit={onEdit} onDelete={onDelete}  />
+        <User
+          key={user.id}
+          id={user.id}
+          name={user.name}
+          capacity={user.capacity}
+          status={user.status}
+          image={user.image}
+          onEdit={onEdit}
+          onDelete={onDelete}
+        />
       ))}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,54 +1,76 @@
-import React, {useState} from 'react';
-import './User.css';
+import React, { useState } from "react";
+import "./User.css";
 
+const User = ({ id, name, capacity, status, image, onEdit, onDelete }) => {
+  const [isEdit, setIsEdit] = useState(false);
 
-const User = ({id, name, capacity, status, image, onEdit, onDelete}) => {
-    const [isEdit, setIsEdit] = useState(false);
+  const handleEdit = () => {
+    setIsEdit(!isEdit);
+  };
 
-    const handleEdit = () => {
-      setIsEdit(!isEdit);
-    };
-    
-    const handleDelete = () => {
-        onDelete(id);
-    }
+  const handleDelete = () => {
+    onDelete(id);
+  };
 
-    const handleOnEditSubmit = (evt) => {
-        evt.preventDefault();
-        onEdit(id, evt.target.name.value, evt.target.capacity.value,evt.target.status.value, evt.target.image.value);
-        setIsEdit(!isEdit);
-      };
+  const handleOnEditSubmit = (evt) => {
+    evt.preventDefault();
+    onEdit(
+      id,
+      evt.target.name.value,
+      evt.target.capacity.value,
+      evt.target.status.value,
+      evt.target.image.value
+    );
+    setIsEdit(!isEdit);
+  };
 
   return (
-      
     <>
-        {isEdit ? (
-      <form onSubmit={handleOnEditSubmit}>
-        <input type="text" placeholder="Name" name="name" defaultValue={name} />
-        <input type="number" placeholder="Capacity" name="capacity" defaultValue={capacity} />
-        <input type="checkbox" placeholder="Status" name="status"  />
-        <input placeholder="Image" name="image" defaultValue={image} />
-        <button className="update-btn" onSubmit={handleOnEditSubmit}>Update</button>
-      </form>
-    ) : (
-      <table>
-        <tbody>
-                <tr>
-                    <td>{id}</td>
-                    <td>{name}</td>
-                    <td>{capacity}</td>
-                    <td>{status===true ? "true" : "false"}</td>
-                    <td><img height="100px" width="100px" src={image} alt="" /></td>
-                    <td>
-                        <button className="update-btn" onClick={handleEdit}>Update</button>
-                        <button className="delete-btn" onClick={handleDelete}>Delete</button>
-                    </td> 
-                </tr>
-                </tbody>
-            </table>    
-    )}
+      {isEdit ? (
+        <form onSubmit={handleOnEditSubmit}>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            defaultValue={name}
+          />
+          <input
+            type="number"
+            placeholder="Capacity"
+            name="capacity"
+            defaultValue={capacity}
+          />
+          <input type="checkbox" placeholder="Status" name="status" />
+          <input placeholder="Image" name="image" defaultValue={image} />
+          <button className="update-btn" onSubmit={handleOnEditSubmit}>
+            Update
+          </button>
+        </form>
+      ) : (
+        <table>
+          <tbody>
+            <tr>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{capacity}</td>
+              <td>{status === true ? "true" : "false"}</td>
+              <td>
+                <img height="100px" width="100px" src={image} alt="" />
+              </td>
+              <td>
+                <button className="update-btn" onClick={handleEdit}>
+                  Update
+                </button>
+                <button className="delete-btn" onClick={handleDelete}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default User;
